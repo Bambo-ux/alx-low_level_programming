3e320e9d1e81d1bd7eedb96ce9ce8ebf09f2b3ee
Bambo-ux <bsonko64@gmail.com>
2022-06-13 13:13:51 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,32 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _atoi - converts a string to an integer.
- * @s: input string.
- * Return: integer.
+ * reset_to_98 - reset the value of integer to 98
+ * @n: Parameter
+ * Return: Always 0.
  */
-int _atoi(char *s)
+void reset_to_98(int *n)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
-
-	while (*(s + count) != '\0')
-	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		if (*(s + count) == '-')
-			pn *= -1;
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
-		{
-			if (size > 0)
-				m *= 10;
-			size++;
-		}
-		count++;
-	}
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	*n = 98;
 }
