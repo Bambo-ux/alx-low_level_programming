1149c8865ca8e1d26ee3d7c37748c30d4a8b1038
Bambo-ux <bsonko64@gmail.com>
2022-06-13 13:11:25 -0700

    Task o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,32 @@
 #include "main.h"
 
 /**
- * swap_int - swap integers
- * @a: first parameter
- * @b: second parameter
- *
- * Description: swaps the values of two integers
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
-
-void swap_int(int *a, int *b)
+int _atoi(char *s)
 {
-	int swap;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	swap = *a;
-	*a = *b;
-	*b = swap;
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+		if (*(s + count) == '-')
+			pn *= -1;
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
